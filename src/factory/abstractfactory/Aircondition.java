f44d3b9a65b5318558f24f5970c047b9326f4702
fengfan <15202849186@163.com>
2023-03-03 09:15:44 +0800

    设计模式开始

--- src/factory/abstractfactory/Aircondition.java
+++
@@ -1,9 +0,0 @@
-package factory.abstractfactory;
-
-/**
- * @author fengfan
- * @date 2023/2/28 17:05
- * @since JDK1.8
- */
-public interface Aircondition {
-}
--- src/factory/abstractfactory/Engine.java
+++
@@ -1,9 +0,0 @@
-package factory.abstractfactory;
-
-/**
- * @author fengfan
- * @date 2023/2/28 17:03
- * @since JDK1.8
- */
-public interface Engine {
-}
